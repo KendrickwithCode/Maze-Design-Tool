a4556a2c821e9dfff43c76cf05725b898d88448c
My Name <76670582+KendrickwithCode@users.noreply.github.com>
2022-04-06 17:46:41 +1000

    Added gridbaglayout with ActionListeners for Buttons

--- src/GUI.java
+++ src/GUI.java
@@ -3,68 +3,120 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class GUI{
+public class GUI implements ActionListener{
 
+    private JTextField width_text, height_text;
+    /**
+     * Adds buttons and labels to the JFrame
+     * @param pane JFrame pane
+     */
+    public void addComponents(Container pane){
+        pane.setLayout(new GridBagLayout());
+        JPanel panel = new JPanel();
+        panel.setBorder(BorderFactory.createTitledBorder("Settings"));
+        GridBagConstraints position = new GridBagConstraints();
+
+        //Label for Width
+        JLabel width = new JLabel("Width: ");
+        position.fill = GridBagConstraints.VERTICAL;
+        position.insets = new Insets(10,10,0,0);
+        position.weightx = 0.5;
+        position.anchor = GridBagConstraints.NORTHWEST;
+        position.gridx = 0;
+        position.gridy = 0;
+        pane.add(width, position);
+
+        //Label for Height
+        JLabel height = new JLabel("Height: ");
+        position.fill = GridBagConstraints.VERTICAL;
+        position.insets = new Insets(10,10,0,0);
+        position.weightx = 0.5;
+        position.anchor = GridBagConstraints.NORTHWEST;
+        position.gridx = 0;
+        position.gridy = 1;
+        pane.add(height, position);
+
+        //Text field for Width
+        width_text = new JTextField("100");
+        position.weightx = 0.5;
+        position.fill = GridBagConstraints.VERTICAL;
+        position.anchor = GridBagConstraints.NORTHWEST;
+        position.gridx = 1;
+        position.gridy = 0;
+        pane.add(width_text, position);
+
+        //Text field for Height
+        height_text = new JTextField("100");
+        position.weightx = 0.5;
+        position.fill = GridBagConstraints.VERTICAL;
+        position.anchor = GridBagConstraints.NORTHWEST;
+        position.gridx = 1;
+        position.gridy = 1;
+        pane.add(height_text, position);
+
+        //Create Button
+        JButton button = new JButton("Create");
+        position.fill = GridBagConstraints.HORIZONTAL;
+        position.anchor = GridBagConstraints.PAGE_END;
+        position.weightx = 0.5;
+        position.gridx = 1;
+        position.gridy = 2;
+        button.addActionListener(this);
+        pane.add(button, position);
+
+        //Export Button
+        button = new JButton("Export");
+        position.fill = GridBagConstraints.HORIZONTAL;
+        position.anchor = GridBagConstraints.PAGE_END;
+        position.weightx = 0.5;
+        position.gridx = 1;
+        position.gridy = 3;
+        pane.add(button, position);
+
+        //Maze Placeholder
+        //button = new JButton("MAZE GOES HERE");
+        //position.weightx = 0.5;
+        //position.fill = GridBagConstraints.REMAINDER;
+       // position.gridx = 4;
+        //position.gridwidth = 2;
+        //position.gridy = 4;
+        //pane.add(button, position);
+    }
+
+
+    /**
+     * Displays the User Interface for the Maze Generation program
+     * attempts to display native design aspects "Look and Feel".
+     */
     public void show_gui() {
+        try{
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException |
+                 UnsupportedLookAndFeelException |
+                 InstantiationException |
+                 IllegalAccessException ignored){
+        }
+
+        //Create frame
         JFrame jframe = new JFrame("Maze Generator");
         jframe.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
+        //Add components to the frame.
+        addComponents(jframe.getContentPane());
 
-        //Progress bar could be useful for when we load mazes in. Add EventListener and it should work.
-        //JProgressBar progressBar = new JProgressBar(0, 100);
-        //progressBar.setValue(50);
-
-        //Set frame size
-        jframe.setPreferredSize(new Dimension(1000, 800));
-
-        //Menu items top left for exporting
-        JMenuBar menuBar = new JMenuBar();
-        JMenu menu = new JMenu("Export");
-        jframe.setJMenuBar(menuBar);
-        menu.add("As .png");
-        menu.add("As .jpg");
-        menuBar.add(menu);
-
-        //This for saving an export
-        //JOptionPane.showMessageDialog(null "File Successfully Saved");
-
-        //final JFileChooser fc = new JFileChooser();
-        //int returnVal = fc.showOpenDialog(this);
-        //if(returnVal==JFileChooser.APPROVE_OPTION){
-        //File file = fc.getSelectedFile();
-        //String filename = file.getAbsolutePath();
-        //} else if(returnVal==JFileChooser.CANCEL_OPTION){
-        //}
-
-        //Frame contents - Specify width/height
-        JPanel panel = new JPanel();
-        JSpinner width_spinner = new JSpinner();
-        JSpinner height_spinner = new JSpinner();
-        width_spinner.setValue(100);
-        height_spinner.setValue(100);
-        panel.setBorder(BorderFactory.createTitledBorder("Settings"));
-        panel.add(new JLabel("Width: "));
-        panel.add(width_spinner );
-        panel.add(new JLabel("Height: "));
-        panel.add(height_spinner);
-
-        //Frame contents - Buttons
-        JButton create = new JButton("Create");
-        JButton clear = new JButton("Clear");
-        JButton solve = new JButton("Solve");
-        create.addActionListener(e -> JOptionPane.showMessageDialog((Component)e.getSource(), "You clicked me"));
-        clear.addActionListener(e -> JOptionPane.showMessageDialog((Component)e.getSource(), "You clicked me"));
-        solve.addActionListener(e -> JOptionPane.showMessageDialog((Component)e.getSource(), "You clicked me"));
-        panel.add(create);
-        panel.add(clear);
-        panel.add(solve);
-
-        jframe.getContentPane().add(panel);
-
+        //Display frame.
         jframe.pack();
-        jframe.setLocationRelativeTo(null);
-
-
         jframe.setVisible(true);
+
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String width = width_text.getText();
+        String height = height_text.getText();
+        int width_number = Integer.parseInt(width);
+        int height_number = Integer.parseInt(height);
+        System.out.println(width_number + " and " + height_number + " are now integers");
     }
 }
