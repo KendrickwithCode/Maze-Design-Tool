5be0a1714096398156f3341a61a91ff6c148dfef
Jason Kendrick <jasonfkendrick@gmail.com>
2022-04-28 14:36:13 +1000

    Added MazeDB and DBConnection Class
    + Added comments to the stubs.
    + Added db.props file to project for future use.

---
+++ src/DBConnection.java
@@ -0,0 +1,55 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DBConnection {
+    /**
+     * The singleton instance of the database connection. For each thread to access DB.
+     */
+    private static Connection instance = null;
+
+    /**
+     * Constructs and Initializes the connection to the Database
+     */
+    private DBConnection(){
+        Properties props = new Properties();
+        FileInputStream in = null;
+        try {
+            in = new FileInputStream("./db.props");
+            props.load(in);
+            in.close();
+
+            // specify the data source, username and password
+            String url = props.getProperty("jdbc.url");
+            String username = props.getProperty("jdbc.username");
+            String password = props.getProperty("jdbc.password");
+            String schema = props.getProperty("jdbc.schema");
+
+            // get a connection
+            instance = DriverManager.getConnection(url + "/" + schema, username,
+                    password);
+        } catch (SQLException | FileNotFoundException sqle) {
+            System.err.println(sqle);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    /**
+     * Provides global access to the singleton instance of the UrlSet. Code from Week 6.
+     *
+     * @return a handle to the singleton instance of the UrlSet.
+     */
+    public static Connection getInstance() {
+        if (instance == null) {
+            new DBConnection();
+        }
+        return instance;
+
+    }
+
+}
---
+++ src/MazeDB.java
@@ -0,0 +1,49 @@
+import javax.swing.*;
+
+public class MazeDB{
+    /**
+     * Constructs and Initialises connection to database
+     */
+    private MazeDB(){
+
+    }
+
+    /**
+     * Attempts to read data saved from previous invocations of the application
+     */
+    public void mazeData(){
+
+    }
+
+    /**
+     * Add a maze to the database.
+     * @param maze The maze to be added.
+     */
+    public void addMaze(Maze maze){
+    }
+
+
+    /**
+     * Delete a Maze from the database.
+     * @param key The maze to be removed.
+     */
+    public void removeMaze(Object key){
+
+    }
+
+
+    /**
+     * Retrieve a Maze from the database.
+     * @param key The maze to be retrieved.
+     */
+    public void getMaze(Object key){
+
+    }
+
+    /**
+     * Close connection to database
+     */
+    public void close(){
+
+    }
+}
